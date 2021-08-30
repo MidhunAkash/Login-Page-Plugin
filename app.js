2dce5c2c81f8d1b5ea59c0e445cb2a327bc52ab4
Malli Midhun Akash <56332768+MIDHUN950@users.noreply.github.com>
2021-08-30 19:41:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+const sLink = document.querySelector("#s-link");
+const lLink = document.querySelector("#l-link");
+const wrapper = document.querySelector(".wrapper");
+const logout = document.querySelector('#lo-button');
+sLink.addEventListener('click', function() {
+    wrapper.style.top = '-100%';
+})
+
+lLink.addEventListener('click', function() {
+    wrapper.style.top = '0%';
+})
+logout.addEventListener('click',function(){
+    auth.signOut();
+})
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV4na1rL9sJfkCicW2ZA2QH2Brm8jz3Zc",
+    authDomain: "login-page-fd3f0.firebaseapp.com",
+    databaseURL: "https://login-page-fd3f0-default-rtdb.firebaseio.com",
+    projectId: "login-page-fd3f0",
+    storageBucket: "login-page-fd3f0.appspot.com",
+    messagingSenderId: "496273769063",
+    appId: "1:496273769063:web:9291d4150a28258e750ac2",
+    measurementId: "G-03G94FYW3R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+
+function register() {
+  const email = document.getElementById('s-email').value
+  const password = document.getElementById('s-password').value
+  const full_name = document.getElementById('s-name').value
+  const cpassword = document.getElementById('c-password').value
+    if(email == ""||password==""||full_name==""||cpassword=="") {
+        ermsg('All feilds are manitory',"serrmsg")
+        return
+    }
+    // Validate input fields
+    if (validate_email(email) == false ) {
+      ermsg('Invalid Email',"serrmsg")
+      return
+      // Don't continue running the code
+    }
+    if (full_name == "") {
+      ermsg('Enter your name',"serrmsg")
+      return
+    }
+    if (validate_pass(password,cpassword) == false) {
+        ermsg('confirm password not matched.',"serrmsg")
+        return
+      }
+   
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        full_name : full_name,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+
+      // DOne
+      window.location.href = "./hom.html"
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+      if(error_message=="Password should be at least 6 characters"){
+        ermsg(error_message,"serrmsg")
+      }
+      else if(error_message="The email address is already in use by another account."){
+          ermsg("User already exist","serrmsg")
+      }
+      else {
+        alert(error_message)
+      }
+    })
+  }
+
+  function login () {
+    // Get all our input fields
+   const email = document.getElementById('l-email').value
+   const password = document.getElementById('l-password').value
+    // Validate input fields
+    if (validate_email(email) == false) {
+      ermsg('Invalid Email',"lerrmsg")
+      return
+      // Don't continue running the code
+    }
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+      window.location.href = "https://login-page-fd3f0.web.app/hom.html"
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+      if(error_message=="The password is invalid or the user does not have a password."){
+        ermsg("Invalid password","lerrmsg")
+      }
+      else if(error_message=="There is no user record corresponding to this identifier. The user may have been deleted."){
+          ermsg("User not found","lerrmsg")
+      }
+      else{
+          alert(error_message)
+      }
+    })
+  }
+
+
+
+function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
+  }
+  
+function validate_pass(pass,cpass) {
+    if(pass == cpass) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+  
+  function ermsg(msg,id) {
+    document.getElementById(id).innerHTML = msg;
+    setTimeout(() => {document.getElementById(id).innerHTML = ""}, 3000);
+}
---
+++ app2.js
@@ -0,0 +1,34 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV4na1rL9sJfkCicW2ZA2QH2Brm8jz3Zc",
+    authDomain: "login-page-fd3f0.firebaseapp.com",
+    databaseURL: "https://login-page-fd3f0-default-rtdb.firebaseio.com",
+    projectId: "login-page-fd3f0",
+    storageBucket: "login-page-fd3f0.appspot.com",
+    messagingSenderId: "496273769063",
+    appId: "1:496273769063:web:9291d4150a28258e750ac2",
+    measurementId: "G-03G94FYW3R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  var fullName = "";
+  auth.onAuthStateChanged((user)=> {
+    if(user) {
+        database.ref('/users/' + user.uid).once('value').then(function(snapshot) {
+            let userData = snapshot.val();
+            fullName = userData.full_name
+            console.log(userData.full_name);
+            document.getElementById("user-name").innerHTML = "Hello, "+ fullName
+            document.getElementById("luser-name").innerHTML = "Hello, "+ fullName
+        })
+    } else {
+        console.log("loged-out")
+    }
+})
+function logout() {
+    auth.signOut()
+    window.location.href = "./index.html"
+}
